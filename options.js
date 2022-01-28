e108d701b6e8c2af05471461404fc1e50e0192dc
Dre Westcook <dre.westcook@octoenergy.com>
2022-01-28 18:25:18 +1100

    Initial Commit
    PoC working

---
+++ options.js
@@ -0,0 +1,24 @@
+chrome.storage.sync.get(
+    {
+        prefix : 'AU',
+        company_key: '',
+    },
+    config => {
+        document.getElementById('prefix-box').value = config.prefix;
+        document.getElementById('company_key-box').value = config.company_key;
+    },
+);
+
+document.getElementById('save-button').onclick = function(){
+    chrome.storage.sync.set(
+        {
+            prefix : document.getElementById('prefix-box').value,
+            company_key: document.getElementById('company_key-box').value
+        },
+        ()=>{
+            document.getElementById('result').innerText = "Saved";
+            setTimeout(function(){
+                document.getElementById('result').innerText = "";
+            },3000);
+        });
+};
